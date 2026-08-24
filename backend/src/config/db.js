4a835fe7340633c4b5291c4c8c46ae9177705cac
ArhamImranQureshi <arhamgameslover@gmail.com>
2026-08-24 06:10:47 +0500

    model added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://arhamgameslover_db_user:HsDjgiOPzl5KWULQ@cluster0.czgzujn.mongodb.net/?appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     }catch(error){
         console.log(error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 connectDB();
 app.route("/api/notes",notesRoutes);
 
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
 
 
 // mongodb+srv://arhamgameslover_db_user:HsDjgiOPzl5KWULQ@cluster0.czgzujn.mongodb.net/?appName=Cluster0
